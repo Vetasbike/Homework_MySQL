074523aab6c38e2658497aa4323959db1ee1f14b
vetasbike <vetasbike@mail.ru>
2022-08-31 17:43:41 +0300

    Домашняя работа к 3 уроку

---
+++ homework_3.sql
@@ -0,0 +1,54 @@
+USE vk;
+
+-- Таблицы под эмоджи
+
+# Категории эмоджи
+
+CREATE TABLE IF NOT EXISTS emoji_versions (
+	emoji_version_id TINYINT NOT NULL AUTO_INCREMENT,
+	emoji_version DECIMAL(2,1,1) UNIQUE NOT NULL,
+	emoji_version_name VARCHAR(20),
+	emojipedia_url_ext VARCHAR(20),
+	PRIMARY KEY (emoji_version_id)
+);
+
+# Виды эмоджи
+
+CREATE TABLE IF NOT EXISTS emoji (
+	emoji_id SMALLINT NOT NULL AUTO_INCREMENT,
+	emoji_name VARCHAR(150),
+	emoji_character VARCHAR(30),
+	emojipedia_url_ext VARCHAR(150) UNIQUE,
+	emojipedia_category VARCHAR(50),
+	codepoint_string VARCHAR(150) UNIQUE,
+	num_codepoints TINYINT,
+	num_platforms_support TINYINT,
+	num_renderings TINYINT,
+	num_changed_renderings TINYINT,
+	hasComponent BOOLEAN DEFAULT NULL,
+ 	unicode_not_recommended BOOLEAN,
+	emoji_version_id TINYINT,
+    PRIMARY KEY (emoji_id),
+    FOREIGN KEY (emoji_version_id) REFERENCES emoji_versions(emoji_version_id)
+);
+
+# Кодировки
+
+CREATE TABLE IF NOT EXISTS codepoints (
+	codepoint_id SMALLINT NOT NULL AUTO_INCREMENT,
+	codepoint VARCHAR(7) NOT NULL UNIQUE,
+	isComponent BOOLEAN DEFAULT NULL,
+	isModifier BOOLEAN DEFAULT NULL,
+	isModifierBase BOOLEAN DEFAULT NULL,
+    PRIMARY KEY (codepoint_id)
+);
+
+CREATE TABLE IF NOT EXISTS emoji_codepoints (
+	emoji_codepoint_id MEDIUMINT NOT NULL AUTO_INCREMENT,
+	emoji_id SMALLINT,
+	codepoint_id SMALLINT,
+	sequence_index TINYINT,
+    PRIMARY KEY (emoji_codepoint_id),
+    FOREIGN KEY (emoji_id) REFERENCES emoji(emoji_id),
+    FOREIGN KEY (codepoint_id) REFERENCES codepoints(codepoint_id)
+);
